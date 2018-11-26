0986b07b906dabb04137f2e31280dc2b0dd5a79b
lukaskurz <lukaskurz5@gmail.com>
2018-11-26 21:19:26 +0100

    added authguard to dashboard

--- standup-sgt-web/src/app/app-routing.module.ts
+++ standup-sgt-web/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './core/auth.guard';
 
 const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "login", component: LoginComponent},
-  { path: "register", component: RegisterComponent}
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "dashboard", component: DashboardComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
