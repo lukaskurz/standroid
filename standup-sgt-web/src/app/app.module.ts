3fe6e43c3762103b8da12fe43e062d32031dc0cf
lukaskurz <lukaskurz5@gmail.com>
2018-11-26 19:42:42 +0100

    added login and register component

--- standup-sgt-web/src/app/app.module.ts
+++ standup-sgt-web/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
