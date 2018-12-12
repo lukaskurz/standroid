4a0c02d04abb44c9a92de0fc68761c1c82b97f00
lukaskurz <lukaskurz5@gmail.com>
2018-12-12 19:53:13 +0100

    fixed canceling in wizard

--- standup-sgt-web/src/app/setup/setup.component.ts
+++ standup-sgt-web/src/app/setup/setup.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'firebase';
 import { AuthService } from '../core/auth.service';
 import { Router } from '@angular/router';
-import { ClrWizard } from '@clr/angular';
+import { ClrWizard, ClrModal } from '@clr/angular';
 
 @Component({
   selector: 'app-setup',
@@ -14,12 +14,27 @@ export class SetupComponent {
   @ViewChild("wizard") wizard: ClrWizard;
   wizardOpen = true;
 
+  @ViewChild("reminder") reminderModal: ClrModal;
+  reminderModalOpen = false;
+
   user: User;
 
   constructor(public auth: AuthService, private router: Router) {
     auth.user.subscribe(user => {
       this.user = user;
     });
+  }
+
+  doCancel() {
+    this.reminderModal.open();
+  }
+
+  closeModal() {
+    this.reminderModal.close();
+  }
+
+  redirectToDashboard() {
+    this.router.navigateByUrl("dashboard");
   }
 
 }
