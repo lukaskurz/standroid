d1c364d001733beb80b4da5b907cf10f660a61bc
lukaskurz <lukaskurz5@gmail.com>
2018-12-12 23:49:31 +0100

    added schedule part to wizard

--- standup-sgt-web/src/app/setup/setup.component.ts
+++ standup-sgt-web/src/app/setup/setup.component.ts
@@ -17,12 +17,47 @@ export class SetupComponent {
   @ViewChild("reminder") reminderModal: ClrModal;
   reminderModalOpen = false;
 
-  user: User;
+  report = {
+    name: "",
+    schedule: {
+      monday: true,
+      tuesday: true,
+      wednesday: true,
+      thursday: true,
+      friday: true,
+      saturday: false,
+      sunday: false,
+      time: null,
+      hour: 9,
+      minute: 30,
+    }
+  };
 
-  constructor(public auth: AuthService, private router: Router) {
-    auth.user.subscribe(user => {
-      this.user = user;
-    });
+  hours: number[] = [];
+  minutes: number[] = [];
+
+  constructor(private router: Router) {
+    for (let counter = 0; counter < 24; counter++) {
+      this.hours.push(counter);
+    }
+    for (let counter = 0; counter < 60; counter++) {
+      this.minutes.push(counter);
+    }
+  }
+
+  anyDayChecked() {
+    if (
+      this.report.schedule.monday ||
+      this.report.schedule.tuesday ||
+      this.report.schedule.wednesday ||
+      this.report.schedule.thursday ||
+      this.report.schedule.friday ||
+      this.report.schedule.saturday ||
+      this.report.schedule.sunday
+    ) {
+      return true;
+    }
+    return false;
   }
 
   doCancel() {
