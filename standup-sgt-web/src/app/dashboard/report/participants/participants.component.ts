a83f9821fe8e13fc63550a0d5d0a750a0be261a8
lukaskurz <lukaskurz5@gmail.com>
2018-12-30 19:34:02 +0100

    added card wrapper to participants and added member add function

--- standup-sgt-web/src/app/dashboard/report/participants/participants.component.ts
+++ standup-sgt-web/src/app/dashboard/report/participants/participants.component.ts
@@ -3,6 +3,11 @@ import { Report } from 'src/app/models/report';
 import { DashboardStorageService } from 'src/app/dashboard-storage.service';
 import { Member } from 'src/app/models/member';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { first } from 'rxjs/operators';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-participants',
@@ -11,10 +16,53 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class ParticipantsComponent implements OnInit {
 
-  constructor(public storage: DashboardStorageService, private afs: AngularFirestore) {
+  isModalOpen = false;
+
+  teamMembers: Member[] = [];
+
+  newTeamMember: Member;
+
+  constructor(
+    public storage: DashboardStorageService,
+    private afs: AngularFirestore,
+    private router: Router,
+    private auth: AuthService,
+    private http: HttpClient
+  ) {
+    this.getTeamMembers();
   }
 
   ngOnInit() {
+  }
+
+  getTeamMembers() {
+    this.auth.user.pipe(first()).toPromise().then(u => {
+      return this.afs.collection<{ team_id: string }>("installations", (ref) => {
+        return ref.where("creator_uid", "==", u.uid).limit(1);
+      }).get();
+    }).then(query => {
+      return query.pipe(first()).toPromise();
+    }).then(snap => {
+      const at = snap.docs[0].get("access_token");
+
+      return this.http.get(`https://slack.com/api/users.list?token=${at}`).pipe(first()).toPromise();
+    }).then((resp: { members: [] }) => {
+      this.teamMembers = resp.members.filter((v1: Member) =>
+        !this.storage.currentReport.selectedMembers.some((v2: Member) => v2.id === v1.id)
+      );
+      const selectUser = {
+        id: "-1",
+        name: "Select a name",
+        color: undefined,
+        deleted: undefined,
+        is_bot: undefined,
+        profile: undefined,
+        real_name: undefined,
+        team_id: undefined
+      };
+      this.newTeamMember = selectUser;
+      this.teamMembers.unshift(selectUser);
+    });
   }
 
   removeMember(member: Member) {
@@ -24,6 +72,28 @@ export class ParticipantsComponent implements OnInit {
       .collection("reports")
       .doc(`${this.storage.currentReport.name}_${this.storage.currentReport.creator_uid}`)
       .update({ selectedMembers: this.storage.currentReport.selectedMembers });
+    this.getTeamMembers();
+  }
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  selectedMemberValid() {
+    if (!!this.newTeamMember) {
+      return this.newTeamMember.id !== "-1";
+    }
+    return false;
+  }
+
+  addTeamMember() {
+    this.storage.currentReport.selectedMembers.push(this.newTeamMember);
+    this.afs
+      .collection("reports")
+      .doc(`${this.storage.currentReport.name}_${this.storage.currentReport.creator_uid}`)
+      .update({ selectedMembers: this.storage.currentReport.selectedMembers });
+    this.isModalOpen = false;
+    this.getTeamMembers();
   }
 
 }
