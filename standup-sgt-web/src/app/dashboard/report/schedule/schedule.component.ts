aba056cb95e8397c18dec9b4ba1f36a5db2557fb
lukaskurz <lukaskurz5@gmail.com>
2018-12-30 20:40:20 +0100

    added schedule changer

--- standup-sgt-web/src/app/dashboard/report/schedule/schedule.component.ts
+++ standup-sgt-web/src/app/dashboard/report/schedule/schedule.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Report } from 'src/app/models/report';
+import { DashboardStorageService } from 'src/app/dashboard-storage.service';
+import { Member } from 'src/app/models/member';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { first } from 'rxjs/operators';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-schedule',
@@ -7,9 +16,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
-  constructor() { }
+  hours: number[] = [];
+  minutes: number[] = [];
+
+  constructor(
+    public storage: DashboardStorageService,
+    private afs: AngularFirestore,
+    private router: Router,
+    private auth: AuthService,
+    private http: HttpClient
+  ) {
+
+    for (let counter = 0; counter < 24; counter++) {
+      this.hours.push(counter);
+    }
+    for (let counter = 0; counter < 60 / 5; counter++) {
+      this.minutes.push(counter * 5);
+    }
+  }
 
   ngOnInit() {
   }
+
+  saveSchedule() {
+    this.afs
+      .collection("reports")
+      .doc(`${this.storage.currentReport.name}_${this.storage.currentReport.creator_uid}`)
+      .update({ schedule: this.storage.currentReport.schedule });
+  }
+
 
 }
