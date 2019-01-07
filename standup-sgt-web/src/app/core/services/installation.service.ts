4c0230fb2506c2498dc63f55a81a44de3423a011
lukaskurz <lukaskurz5@gmail.com>
2019-01-07 08:22:10 +0100

    added another install guard

--- standup-sgt-web/src/app/core/services/installation.service.ts
+++ standup-sgt-web/src/app/core/services/installation.service.ts
@@ -1,15 +1,14 @@
-import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
-import { Installation } from '@app/shared/models/installation';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { UserService } from './user.service';
-import 'rxjs/add/operator/map';
+import { Injectable } from "@angular/core";
+import { ReplaySubject } from "rxjs";
+import { Installation } from "@app/shared/models/installation";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { UserService } from "./user.service";
+import "rxjs/add/operator/map";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class InstallationService {
-
   private installation: ReplaySubject<Installation>;
 
   constructor(private afs: AngularFirestore, private us: UserService) {
@@ -19,13 +18,37 @@ export class InstallationService {
 
   private setInstallation() {
     this.us.getUser().subscribe(user => {
-      this.afs.collection<Installation>("installations", ref => {
-        return ref.where("creator_uid", "==", user.uid).limit(1);
-      }).valueChanges().map(val => val[0]).subscribe(this.installation);
+      this.afs
+        .collection<Installation>("installations", ref => {
+          return ref.where("creator_uid", "==", user.uid).limit(1);
+        })
+        .valueChanges()
+        .map(val => val[0])
+        .subscribe(this.installation);
     });
   }
 
   getInstallation() {
     return this.installation.asObservable();
+  }
+
+  hasInstallation() {
+    return new Promise((res, rej) => {
+      this.us.getUser().subscribe(user => {
+        this.afs
+          .collection<Installation>("installations", ref => {
+            return ref.where("creator_uid", "==", user.uid).limit(1);
+          })
+          .get()
+          .map(snapshot => snapshot.size)
+          .subscribe(amount => {
+            if (amount > 0) {
+              res(true);
+            } else {
+              res(false);
+            }
+          });
+      });
+    });
   }
 }
